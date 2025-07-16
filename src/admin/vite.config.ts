39a5f0a124811c882aed787467ea69c18c379bd4
mihirniyogi <e1122052@u.nus.edu>
2025-07-16 21:02:15 +0800

    Go-around fix strapi types issues

--- src/admin/vite.config.ts
+++ src/admin/vite.config.ts
@@ -3,6 +3,9 @@ import { mergeConfig, type UserConfig } from 'vite';
 export default (config: UserConfig) => {
   // Important: always return the modified config
   return mergeConfig(config, {
+    server: {
+      allowedHosts: ['strapi2.mihirniyogi.com', '192.168.0.4', 'localhost']
+    },
     resolve: {
       alias: {
         '@': '/src',
